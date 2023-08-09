f96465252d7cacb1cc55b622ad6d715936031728
rabia1311 <rabiasultana.n@gmail.com>
2023-08-09 16:20:19 +1030

    connected socketio

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
-const socketID = require("socket.io");
+const socketIO = require("socket.io");
 const app = express();
 const port = 4500 || process.env.PORT;
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello its working");
 });
 
 const server = http.createServer(app);
-const io = socketID(server);
+const io = socketIO(server);
+
+io.on("connection", () => {
+  console.log("New Connection");
+});
+
 server.listen(port, () => {
   console.log(`server is working on http://localhost:${port}`);
 });
